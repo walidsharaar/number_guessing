0b2c91c35ed5b51183751b11062fa750fb734840
Sharaar <walidsharar@gmail.com>
2021-06-05 13:32:58 +0200

    Project Completed

--- main.py
+++ main.py
@@ -1,48 +1,71 @@
-# display welcome msg to the user
-
 from random import randint
 from art import  logo
 
-EASY_ATTEMPT = 5
-HARD_ATTEMPT = 10
+EASY_ATTEMPT = 10
+HARD_ATTEMPT = 5
+
+
+#function to check user guess number with computer generated number
+
+def check(answer , guess ,turns):
+    """Check answer compare to guess and return remaining turns"""
+
+    if guess>answer:
+        print(f"{guess} is too high compared to guess number.")
+        return turns -1
+    elif answer>guess:
+        print(f"{guess} is too low compared to guess number.")
+        return turns -1
+    else:
+        print(f"You got it ! the answer was {guess} .")
 
 #function to ask the difficulties
 
 def difficuly():
-    level = input("Choose the difficulty level: Type 'Easy' or 'Hard' ").lower()
+    level = input("Choose the difficulty level: Type 'Easy' or 'Hard'   ").lower()
     if level == 'easy':
-        print(f"You have {EASY_ATTEMPT} attempts. ")
-        turns = EASY_ATTEMPT
+        return EASY_ATTEMPT
     elif level=='hard':
-        print(f"You have {HARD_ATTEMPT} attempts. ")
-        turns = HARD_ATTEMPT
+        return HARD_ATTEMPT
     else :
         print("Invalid Input !")
         return 0
 
-#function to check user guess number with computer generated number
 
-def check(answer , guess):
-    if guess>answer:
-        print(f"{answer} is too high compared to guess number.")
-    elif answer>guess:
-        print(f"{answer} is too low compared to guess number.")
-    else:
-        print(f"You got it ! the answer was {answer} .")
+def play_game():
 
-print(logo)
+    print(logo)
+    print("Welcome to Guessing Game")
+    print("Think a number between 1 to 100! ")
 
-print("Welcome to Guessing Game")
-print("Think a number between 1 to 100! ")
+    #pick the number between 1 to 100
+
+    answer = randint(1,100)
+    print(f"Actual number is {answer}")
+
+    turns = difficuly()
+    guess =0
+    while guess !=answer:
+        print(f"You have {turns} attempts. ")
+        #store the guess number
+        guess = int(input("What is your guess: "))
+
+        # check number of turns and substract by 1 in each wrong guess
+
+        turns = check(guess,answer,turns)
+        if turns == 0 :
+            print("You can't guess more. You lose.")
+            return
+        elif guess !=answer :
+            print("Guess once again. ")
 
 
-#select the number
-answer = randint(1,100)
 
-#store the guess number
 
-guess = int(input("What is your guess: "))
 
+# call the start function
+
+play_game()
 
 
 
